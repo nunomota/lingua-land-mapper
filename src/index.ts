afdfb80f1f6a4c97a1003b0a1bc5c5e3a897cd7e
Nuno Mota <nunomota@users.noreply.github.com>
2026-05-09 00:13:32 +0200

    feat: register prop routes in app

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,16 @@ import "dotenv/config";
 import express from "express";
 import mapStreamRouter from "./routes/mapStream.js";
 import mapRouter from "./routes/map.js";
+import propsStreamRouter from "./routes/propStream.js";
+import propsRouter from "./routes/props.js";
 
 const app = express();
 app.use(express.json());
 
 app.use("/map", mapStreamRouter);
 app.use("/map", mapRouter);
+app.use("/props", propsStreamRouter);
+app.use("/props", propsRouter);
 
 const PORT = process.env.PORT ?? 3000;
 app.listen(PORT, () => {
