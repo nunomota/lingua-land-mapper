bf7668a8bcb063d334e78b77db2cc4b7599d744f
Nuno Mota <nunomota@users.noreply.github.com>
2026-05-09 16:53:00 +0200

    refactor: update entry point to use detail routes

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,16 @@ import "dotenv/config";
 import express from "express";
 import mapStreamRouter from "./routes/mapStream.js";
 import mapRouter from "./routes/map.js";
-import propsStreamRouter from "./routes/propStream.js";
-import propsRouter from "./routes/props.js";
+import detailsStreamRouter from "./routes/detailStream.js";
+import detailsRouter from "./routes/details.js";
 
 const app = express();
 app.use(express.json());
 
 app.use("/map", mapStreamRouter);
 app.use("/map", mapRouter);
-app.use("/props", propsStreamRouter);
-app.use("/props", propsRouter);
+app.use("/details", detailsStreamRouter);
+app.use("/details", detailsRouter);
 
 const PORT = process.env.PORT ?? 3000;
 app.listen(PORT, () => {
